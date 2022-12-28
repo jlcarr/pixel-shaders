43bf1bd761284b6d3dce30de210d0814cb2b926a
Joseph Carr <joseph.carr@mail.mcgill.ca>
2022-12-28 00:16:49 -0500

    A new circuit animation using revised vector field techniques and new methods for timing and drawing.

--- circuit.glsl
+++ circuit.glsl
@@ -4,16 +4,25 @@ precision mediump float;
 
 #define M_PI 3.1415926535897932384626433832795
 #define EPSILON 0.01
+const float discrete_sweep = 2. * M_PI / 8.;
 
+// inputs
 uniform vec2 u_resolution;
 uniform float u_time;
-const int grid_size = 16;
-const float line_width = 0.2;
-const float grid_width = 0.1;
 
+// params
+const int grid_num_cells = 18;
+const float grid_line_width = 0.05;
+const float global_line_width = grid_line_width / float(grid_num_cells);
+const float line_width = 0.25;
 
-int imod(int a, int b) {
-    return a - b*int(a/b);
+// computed
+const float grid_cell_width = 2.0/float(grid_num_cells);
+
+// primitives
+mat2 rot2d(float theta){
+    return mat2(cos(theta),-sin(theta),
+                sin(theta),cos(theta));
 }
 
 float rand(vec2 st) {
@@ -21,190 +30,190 @@ float rand(vec2 st) {
     return fract(sin(dot(st,vec2(12.34,56.78)))*81.011);
 }
 
-
-// Draw Primitives
-float draw_line(vec2 pos, vec2 dir){
-    vec2 proj = dot(pos, dir) / length(dir) * normalize(dir);
-    vec2 dist = pos - proj;
-    return 1.0 - step(line_width, length(dist));
+// Draw functions
+float draw_grid(vec2 grid_cell_pos){
+    vec2 bounds = step(1.-grid_line_width/2., abs(grid_cell_pos));
+    return float(bool(bounds.x) || bool(bounds.y));
 }
 
-float draw_anulus(float orig, vec2 pos, vec2 center){
+float circle(vec2 pos, vec2 center, float r){
+    return 1. - step(r, length(pos-center));
+}
+
+float anulus(vec2 pos, vec2 center){
     float dist = length(pos-center);
-    if (dist < line_width * 3.0 / 2.0) orig = 1.0;
-    if (dist < line_width / 2.0) orig = 0.0;
-    return orig;
+    return step(dist, line_width) * step(line_width/2., dist);
 }
 
-float draw_line_animated(vec2 pos, vec2 dir, float t){
-    vec2 proj = dot(pos, dir) / length(dir) * normalize(dir);
-    float result = 1. - step(line_width, length(pos - proj)); // px is on the line
-    result *= step(0., dot(pos, dir)); // is past the center
-    result *= 1.-step(t*dot(dir, dir), dot(pos, dir)); // timer
-    
-    //result = draw_anulus(result, pos, t*dir);
+float line_seg(vec2 pos, vec2 start, vec2 end){
+    vec2 dir = normalize(end-start);
+    float proj_len = dot(pos-start, dir);
+    vec2 proj = proj_len * dir;
+    vec2 dist = (pos-start) - proj;
+    float result = 1.0 - step(line_width / 2., length(dist));
+    result *= step(0., proj_len) * step(0., length(end-start)-proj_len);
     return result;
 }
 
-float draw_grid(vec2 pos){
-    return 1. - step(grid_width/2., 1.-abs(pos.x)) * step(grid_width/2., 1.-abs(pos.y));
+vec3 float2color(float v){
+    return vec3(step(0.,v)*v, 0., -step(0.,-v)*v);
 }
 
-
-// Utility
-float rad_theta(ivec2 grid_coord){
-    return atan(float(2*grid_coord.y - grid_size+1)/2.0, float(2*grid_coord.x - grid_size+1)/2.0);
+vec2 discretize_dir(vec2 dir){
+    float theta = atan(dir.y,dir.x);
+    float discrete_theta = floor(theta / discrete_sweep + 0.5) * discrete_sweep; // round to nearest multiple
+    return vec2(cos(discrete_theta), sin(discrete_theta));
 }
 
-float discretize_theta(float theta){
-    return floor(theta * 8.0 / (2.0 * M_PI) + 0.5) * 2.0 * M_PI / 8.0;
+// field operations
+// default fields
+vec2 rad_field(vec2 pos){
+    return pos;
 }
 
-float grid_rad(ivec2 grid_coord){
-    vec2 pos_vec = vec2(2*grid_coord - grid_size+1)/2.0;
-    return length(pos_vec);
+vec2 rotating_field(vec2 pos){
+    return vec2(cos(u_time),sin(u_time));
 }
 
-ivec2 theta_to_discrete_dir(float theta){
-    return ivec2((1.0+EPSILON)*sqrt(2.0)*vec2(cos(theta), sin(theta)));
+vec2 rotational_field(vec2 pos){
+    return vec2(-pos.y,pos.x);
 }
 
+float sinusoidal_field(vec2 pos){
+    pos *= 1.;
+    //pos += vec2(u_time);
+    //pos += vec2(cos(u_time), sin(u_time));
+    return sin(2. * M_PI * pos.x / float(grid_num_cells)) * sin(2. * M_PI * pos.y / float(grid_num_cells)); //* sin(u_time);
+}
 
-// Vector fields
-vec2 rad_dir(ivec2 grid_coord){
-    float theta = rad_theta(grid_coord);
-    theta = discretize_theta(theta);
+// computed fields
+float scalar_field(vec2 pos){
+    return sinusoidal_field(pos);
+}
+
+vec2 gradient_field(vec2 pos){
+    float h = EPSILON;
+    vec2 dx = vec2(h, 0.);
+	vec2 dy = vec2(0., h);
+    return vec2(
+        scalar_field(pos+dx) - scalar_field(pos-dx),
+        scalar_field(pos+dy) - scalar_field(pos-dy)
+    ) / (2. * h); // central difference
+}
+
+vec2 stear_random_field(vec2 pos){
+    const float range = 3. * M_PI / 8.;
+    vec2 dir = rad_field(pos); // replace with any vector field
+    float theta = atan(dir.y,dir.x);
+    float offset = (rand(pos)-0.5) * range;
+    theta += offset;
     return vec2(cos(theta), sin(theta));
 }
 
-vec2 NS_field(ivec2 grid_coord){
-    return vec2(0.0, 1.0);
+vec2 vector_field(vec2 pos){
+    return stear_random_field(pos); // replace with any vector field
+    //return rad_field(pos);
+    //return rotational_field(pos);
+    //return gradient_field(pos);
+	//return rotating_field(pos);
 }
 
-vec2 rot_field(ivec2 grid_coord){
-    float theta = rad_theta(grid_coord);
-    theta = discretize_theta(theta);
-    return vec2(sin(theta), -cos(theta));
+float metric(vec2 pos){
+    float dist = max(abs(pos.x),abs(pos.y)); // choose metric
+    dist = length(pos);
+    return dist;
 }
 
-vec2 wave_field(ivec2 grid_coord){
-    vec2 dir = vec2(sin(float(grid_coord.y) * M_PI / 4.0),1.0);
-    float theta = atan(dir.y, dir.x);
-    theta = discretize_theta(theta);
-    return vec2(cos(theta), sin(theta));
-}
-
-vec2 dicontinuous_wave_field(ivec2 grid_coord){
-    vec2 v = wave_field(grid_coord);
-    v *= (1.- 2.*step(3., float(imod(grid_coord.y, 4))));
-    return v;
-}
-
-vec2 random_NS_field(ivec2 grid_coord){
-    vec2 result = vec2(0.0, 1.0);
-    float random_v = rand(vec2(grid_coord));
-
-    result += vec2(1., 0.) * step(0.5,random_v);
-    return result;
-}
-
-vec2 random_rad_dir(ivec2 grid_coord){
-    float theta = rad_theta(grid_coord);
-    theta = discretize_theta(theta);
+float draw_discrete_streamlines(ivec2 grid_cell_coord, vec2 grid_cell_pos){
+    const float period = float(grid_num_cells);
+    const float head_length = 1./2.;
+    float result = 0.;
+    // setup
+    vec2 pos = vec2(grid_cell_coord);
+    pos = pos - .5 * sign(pos);
+    float dist = metric(pos);
+    float cell_time = clamp(mod(u_time, period) - dist, 0., 1.0);
     
-    float random_v = rand(vec2(grid_coord));
-    theta += 2.0 * M_PI / 8.0 * (step(0.9, random_v) + step(0.1, random_v) - 1.);
-    
-    return vec2(cos(theta), sin(theta));
-}
-
-vec2 vector_field(ivec2 grid_coord){
-    return random_rad_dir(grid_coord);
-}
-
-
-// Main drawing
-float draw_discrete_vector_field(ivec2 grid_coord, vec2 grid_pos){
-    // timing
-    float t = mod(u_time, float(1+grid_size/2));
-    vec2 float_grid_coord = vec2(2*grid_coord - grid_size+1)/2.0;
-    float r = max(abs(float_grid_coord.x), abs(float_grid_coord.y));
-    float curr_t = clamp(2.*(t-r-.5)*step(0., t-r-.5), 0., 2.);
-    float curr_t2 = clamp(2.*(t-r+.5)*step(0., t-r+.5), 0., 2.);
-    
-    // current vector
-    vec2 dir = normalize(vector_field(grid_coord));
-    float line_len = length(vec2(bool(dir.x), bool(dir.y)));
-    float drawing = draw_line_animated(grid_pos, line_len*dir, curr_t);
-    drawing *= step(0., dot(grid_pos,dir)); // only point past the center
-    //drawing *= 0.0;
-    
-    float entered = 0.0;
-    
+	bool entered = false;
     
     for (int i = -1; i <= 1; i++){
         for (int j = -1; j <= 1; j++){
-            ivec2 offset = ivec2(i,j);
-            vec2 dir2 = normalize(vector_field(grid_coord + offset));
-            float line_len2 = length(vec2(bool(dir2.x), bool(dir2.y)));
-            //float drawing2 = draw_line(grid_pos-2.*vec2(offset), dir2);
-            float drawing2 = draw_line_animated(grid_pos-2.*vec2(offset), line_len2*dir2, curr_t2);
-            drawing2 *= step(0., dot(grid_pos,normalize(vec2(offset)))); // only point past the center
-            drawing2 *= step(0., dot(dir2,-vec2(offset)));
-            drawing2 *= step(EPSILON, length(vec2(offset)));
-            drawing = max(drawing, drawing2);
-            
-            entered += step(1.-EPSILON, dot(dir2, -normalize(vec2(offset)))) //
-                * (1.-step(1.-EPSILON, dot(-dir2, dir)));
-    	}
+            // move to cell
+            vec2 pos_offset = vec2(i,j);
+            vec2 offset_pos = pos + pos_offset;
+            vec2 grid_cell_pos_offset = 2.*vec2(ivec2(i,j));
+            // animate
+            float offset_dist = metric(offset_pos);
+            float offset_cell_time = clamp(mod(u_time, period) - offset_dist, 0., 1.0);
+            float is_drawn = float(offset_cell_time > 0.);
+            // the actual vector field
+            vec2 dir = vector_field(offset_pos);
+            dir = discretize_dir(dir);
+            dir = 2.*sign(dir)*ceil(abs(dir)); // round to hit centers of neighboring cells
+            entered = entered || (length(pos_offset+dir/2.) < EPSILON);
+            dir *= offset_cell_time;
+            vec2 offset_dir = dir+grid_cell_pos_offset;
+            // drawing
+            result = max(result, line_seg(grid_cell_pos, grid_cell_pos_offset, offset_dir));
+            //caps
+            result = max(result, is_drawn*circle(grid_cell_pos, grid_cell_pos_offset, line_width / 2.));
+            result = max(result, is_drawn*circle(grid_cell_pos, offset_dir, line_width / 2.));
+        }
     }
+    // logic: if none point into a cell, grow a node when is time.
+    // leave a node while it is not yet the cell's time
+    for (int i = -1; i <= 1; i++){
+        for (int j = -1; j <= 1; j++){
+            // move to cell
+            vec2 pos_offset = vec2(i,j);
+            vec2 offset_pos = pos + pos_offset;
+            vec2 grid_cell_pos_offset = 2.*vec2(ivec2(i,j));
+            // animate
+            float offset_dist = metric(offset_pos);
+            float offset_cell_time = clamp(mod(u_time, period) - offset_dist, 0., 1.0);
+            float is_drawn = float(offset_cell_time > 0.);
+            // the actual vector field
+            vec2 dir = vector_field(offset_pos);
+            dir = discretize_dir(dir);
+            dir = 2.*sign(dir)*ceil(abs(dir)); // round to hit centers of neighboring cells
+            entered = entered || (length(pos_offset+dir/2.) < EPSILON);
+            dir *= offset_cell_time;
+            vec2 offset_dir = dir+grid_cell_pos_offset;
+            // heads
+            is_drawn *= float((cell_time <= 0. && !(i == 0 && j == 0)) || (offset_cell_time < 1. && !(i == 0 && j == 0)) || (i == 0 && j == 0));
+            result = max(result, is_drawn*circle(grid_cell_pos, offset_dir, line_width)) * step(is_drawn*line_width/2.,length(offset_dir-grid_cell_pos));
+        }
+    }
+    float past_cell_time = clamp(8.*(mod(u_time+.125, period) - dist), 0., 1.0);
+    float is_drawn = float(!entered && past_cell_time > 0.);
+    result = max(result, is_drawn*circle(grid_cell_pos, vec2(0.), past_cell_time*line_width)) * step(is_drawn*past_cell_time*line_width/2.,length(grid_cell_pos));
     
-
-    // anulus
-    entered = step(EPSILON, entered);
-    entered = max(entered, 1.-step(0., t-r-.5));
-    drawing = max(drawing, step(0.,line_width-length(grid_pos))*step(0., t-r-.5));
-    float annus = draw_anulus(drawing, grid_pos, vec2(0.0));
-    drawing = (1.-entered) * annus + entered*drawing;
-    
-    return drawing;
+    return result;
 }
-
 
 void main(void) {
 	vec2 pos = gl_FragCoord.xy/u_resolution.xy;
-	pos = pos*2.0 - 1.0;
+	pos = pos * 2. - 1.; // convert to [-1,1]
 
-    // grid up
-    float grid_length = 2.0/float(grid_size);
-    ivec2 grid_coord = ivec2((pos+1.0)/grid_length);
-    vec2 grid_pos = mod(pos, grid_length);
-    grid_pos = (grid_pos - grid_length/2.0)*float(grid_size);
+    // get grid
+    ivec2 grid_cell_coord = ivec2(pos/grid_cell_width + sign(pos));
+    vec2 grid_cell_pos = mod(pos, grid_cell_width) / grid_cell_width;
+    grid_cell_pos =  grid_cell_pos * 2. - 1.; // convert to [-1,1]
     
-    
-    // main drawing
-    float drawing = draw_discrete_vector_field(grid_coord, grid_pos);
-    
-    
-    float rad = mod(u_time, float(grid_size/4));
-    //drawing *= 1.-step(rad, abs(pos.x)+abs(pos.y));
-    
-    
-    //draw grid;
-    float grid_drawing = draw_grid(grid_pos);
-    drawing = max(drawing, grid_drawing);
-    
-    // circle boundary
-    /*
-    int rad = int(mod(u_time, float(grid_size)));
-    drawing *= 1.-step(float(rad), grid_rad(grid_coord));
-    drawing *= 1.-0.5*step(float(rad)/float(grid_size/2), length(pos));
-    */
-    
-    
+    //draw
     vec3 color = vec3(0.);
-    color += vec3(1.-drawing);
+    
+    // field
+    //color = float2color(scalar_field(pos/grid_cell_width));
+    color = max(color, draw_discrete_streamlines(grid_cell_coord, grid_cell_pos));
+    
+    // draw grid
+    color = max(color, draw_grid(grid_cell_pos));
+    //pos = rot2d(u_time/8.) * pos;
+    //color = max(color, vec3(.0,.0,1.)*sin(2.*M_PI*pos.x)*sin(2.*M_PI*pos.y)*sin(u_time));//*sin(u_time));
+    //color = max(color, vec3(.0,.0,1.));
     
 	gl_FragColor = vec4(color, 1.0);
 }
+
 
